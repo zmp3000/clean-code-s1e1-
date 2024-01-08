055e246a98b1ac88c24cb30b1619390ed04f4bcd
zmp3000 <asemenuk100@gmail.com>
2024-01-08 21:19:06 +0300

    fix(extended-1.2): add alt text to img

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.setAttribute('alt', 'delete button');
     deleteButton.appendChild(deleteButtonImg);
 
 
